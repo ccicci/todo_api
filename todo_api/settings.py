56e64bb03b6d86f8d29ffb08d109e182b9dc826a
ccicci <kongyeesam@gmail.com>
2021-05-20 12:01:47 +0900

    update setting.py, urls.py

--- todo_api/settings.py
+++ todo_api/settings.py
@@ -28,13 +28,14 @@ ALLOWED_HOSTS = []
 
 # Application definition
 INSTALLED_APPS = [
-    'todos',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todos',
+    'rest_framework',
 ]
 
 REST_FRAMEWORK = {
