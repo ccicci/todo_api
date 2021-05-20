d57103cc5bc67738a8251c7d7aff00508dbf7eea
ccicci <kongyeesam@gmail.com>
2021-05-20 10:47:05 +0900

    update models.py

--- todos/models.py
+++ todos/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+class Todo(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name='번호')
+    title = models.CharField(max_length=126, null=False, verbose_name='제목')
+    description = models.TextField(null=False, verbose_name='내용')
+    is_completed = models.BooleanField(default=False, verbose_name='완료여부')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='작성날짜')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='수정날짜')
+
+    class Meta:
+        ordering = ['-created_at']
+
+
+class Comment(models.Model):
+    id = models.AutoField(primary_key=True, verbose_name='번호')
+    contents = models.TextField(null=False, verbose_name='내용')
+    created_at = models.DateTimeField(auto_now_add=True, verbose_name='작성날짜')
+    updated_at = models.DateTimeField(auto_now=True, verbose_name='수정날짜')
+    todo_id = models.ForeignKey(Todo, on_delete=models.CASCADE)
+
+    class Meta:
+        ordering = ['-created_at']
+
